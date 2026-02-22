7285139a5b07b5ee2bc5711fb08807bbde3d18cd
rrk24s036 <rahmatulla.r08@gmail.com>
2026-02-22 16:40:38 +0300

    documentation for Shared

--- Shared/Dtos/ArticleDto.cs
+++ Shared/Dtos/ArticleDto.cs
@@ -1,5 +1,14 @@
 namespace Shared.Dtos;
 
+/// <summary>
+/// Represents a dto containing parsed article information.
+/// </summary>
+/// <param name="Id">Unique article identifier.</param>
+/// <param name="URL">Source URL of the article.</param>
+/// <param name="Title">Article title. Null if not found.</param>
+/// <param name="Tags">List of article tags.</param>
+/// <param name="Metatags">List of meta tag attributes.</param>
+/// <param name="Links">Dictionary of extracted links.</param>
 public record ArticleDto
 (
     string Id,
--- Shared/Dtos/AurPackageDto.cs
+++ Shared/Dtos/AurPackageDto.cs
@@ -1,5 +1,13 @@
 namespace Shared.Dtos;
 
+/// <summary>
+/// Represents a dto containing parsed AUR package information.
+/// </summary>
+/// <param name="ID">Unique package identifier.</param>
+/// <param name="Name">Package name</param>
+/// <param name="Version">Package version. Null if not found.</param>
+/// <param name="URL">Source URL of the package. Null if not found.</param>
+/// <param name="Description">Package description. Null if not found.</param>
 public record AurPackageDto
 (
     int ID, 
